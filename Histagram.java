2432d0b4f1d5fd6c54581cc6845992b3d771e0d2
DKuwalek <d.kuwalek@gmail.com>
2014-07-14 15:06:23 +0200

    Histagram
    Done.

--- Histagram.java
+++ Histagram.java
@@ -1,34 +1,54 @@
 public class Histagram{
 	
-	final char JUICE='G';
-	final char BOX=' ';
+	public static void main(String[] args){
 	
-	public void run(){
+		Histagram Toblerone = new Histagram();
+		Toblerone.run(10, 'A', ' ');
+	
+	}
+	
+	public void run(int height, char sign, char spacing){
 		
-		//Setting up height
-		for(int h=1; h<5; h++){
-			//Output of row width
-			//System.out.println("Out" + h));
-			int row=0;
+		//Fields
+		//rowLength = Number of signs in every line
+		int rowLength = 2 * height - 1;
+		
+		////
+		//Giving out passed info
+		System.out.println("Height given: "  + height);
+        System.out.println("sign given: "    + sign);
+        System.out.println("spacing with: "  + spacing);
+		//System.out.println("Sign counter = " + rowLength);
+		
+		//Main loop - height times
+        System.out.println("Entering main loop");		
+		for (int a = 1; a <= height; a++)
+		{
+			int signsInRow = 2 * a - 1;
+			int spacingsInRow = rowLength - signsInRow;
+			//System.out.println("height= " + a);
 			
-			//Filling row
-			while(row<(h)){
-				System.out.print(JUICE);
-				row++;
+			//printing chosen white spacings - left half of it
+			for (int b = 1; b <= spacingsInRow / 2 ; b++)
+			{
+		        //System.out.println("inside loop b= " + b + "\nspacings in row / 2 =" + (spacingsInRow / 2));
+				System.out.print(spacing);
 			}
-			
-			//Spacing for new row
+			//printing chosen sign - all of it
+			for (int z = 1; z <= signsInRow; z++)
+			{
+				System.out.print(sign);
+			}
+			//printing chosen white spacings - right half of it
+			for (int c = 1; c <= spacingsInRow / 2; c++)
+			{
+		        System.out.print(spacing);
+			}						
+			//Switching to the next row
 			System.out.println();
+			
 		}
 	
 	}
 	
-	public static void main(String[] args){
-	
-		Histagram Camel = new Histagram();
-		Camel.run();
-	
-	}
-
-
 }
